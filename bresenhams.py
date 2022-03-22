8898ea1cb3d1b89cb994f26d23c300d63b5fa3e7
Komal <101037687+komalrana36@users.noreply.github.com>
2022-03-22 08:44:08 +0500

    Add files via upload

---
+++ bresenhams.py
@@ -0,0 +1,48 @@
+def f(x):
+    return x**3-5*x-9
+def g(x):
+    return 3*x**2-5
+def newtonRaphson(x0,N):
+    print('\n\n*** Newton Raphson Method implementation****')
+    step=1
+    flag=1
+    condition=true
+    while condition:
+        if g(x0)==0.0:
+            print('divide by zero error!')
+            break
+        x1 = x0 - f(x0)/g(x0)
+        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, x1, f(x1)))
+        x0 = x1
+        step = step + 1
+        
+        if step > N:
+            flag = 0
+            break
+        
+        condition = abs(f(x1)) > e
+    
+    if flag==1:
+        print('\nRequired root is: %0.8f' % x1)
+    else:
+        print('\nNot Convergent.')
+#input section
+        x0 = input('Enter Guess: ')
+e = input('Tolerable Error: ')
+N = input('Maximum Step: ')
+
+# Converting x0 and e to float
+x0 = float(x0)
+e = float(e)
+
+# Converting N to integer
+N = int(N)
+
+
+#Note: You can combine above three section like this
+# x0 = float(input('Enter Guess: '))
+# e = float(input('Tolerable Error: '))
+# N = int(input('Maximum Step: '))
+
+# Starting Newton Raphson Method
+newtonRaphson(x0,e,N)
